22dc8ee38267f10ec76a7fdba06cc2a541245c1f
Carl <carl.mikaelsson@chasacademy.se>
2025-05-14 21:22:52 +0200

    loredump

---
+++ src/pages/Lore.tsx
@@ -0,0 +1,18 @@
+const Lore = () => {
+    return (
+        <div>
+            <h2>A Minor List of Lore excerpts</h2>
+            <ul>
+                <li>
+                    <p>Dwarves: The underground cousins of the elves. Also known as dark elves, many falsely believe dark elves and dwarves to be different.
+                        The true reason for the "dark" adjective comes from the fact that they are covered in soot and coal.
+                        In actuality, their skin color rivals that of snow due to lack of sunlight underground.
+                        The pressure of the deep have also compressed their build giving them a more muscular and compact build.
+                    </p>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Lore;
